2a968db98452d6b51419a3fb6c267ec457184a27
cblair1986@gmail.com <cblair1986@gmail.com>
2020-03-19 18:36:50 -0400

    more fiddling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
     println!("Hello, world!");
 }
+
+/* So the main idea, then, is to do something interesting with this. Perhaps grids, board games, etc. Who knows, currently? Not me. I'll figure it out
+or I won't. We'll see.
+*/
