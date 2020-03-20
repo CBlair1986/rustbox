0e5dd4f2797bfd69fb8979c8874a02db79ca2799
Christopher Blair <CBlair@jmsep.com>
2020-03-20 10:22:08 -0400

    line length change

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ fn magic_sum(n: i32) -> i32 {
     (n.pow(4) + n.pow(2)) / (2*n)
 }
 
-/* So the main idea, then, is to do something interesting with this. Perhaps grids, board games, etc. Who knows, currently? Not me. I'll figure it out
+/* So the main idea, then, is to do something interesting with this. Perhaps
+grids, board games, etc. Who knows, currently? Not me. I'll figure it out
 or I won't. We'll see.
 */
