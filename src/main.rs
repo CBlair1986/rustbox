5f5a5ae61b745b00670ef8bfe4169b43586049d1
cblair1986@gmail.com <cblair1986@gmail.com>
2020-03-20 10:00:01 -0400

    pivoting to magic squares

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Magic Sums:");
+    for n in 3..10 {
+        println!("f({}) = {}", n, magic_sum(n));
+    }
+}
+
+fn magic_sum(n: i32) -> i32 {
+    (n.pow(4) + n.pow(2)) / (2*n)
 }
 
 /* So the main idea, then, is to do something interesting with this. Perhaps grids, board games, etc. Who knows, currently? Not me. I'll figure it out
